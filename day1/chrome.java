9dab42c71f71864e60c997d5e7035992e8379263
Ajithkumar2197 <ajithkumar.amaravathy@gmail.com>
2024-12-09 15:05:09 +0530

    Add files via upload

---
+++ day1/chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.print("chrome version is "+ version);
+		
+		System.out.print(" developed by "+ developer);
+		
+		System.out.print(" Is it Beta "+ isBeta+"\nreleased on "+ releaseYear);
+		System.out.println(shortcutKey);
+		
+	}
+}
